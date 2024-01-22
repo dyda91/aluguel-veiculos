da5af1470ad2cd0ef7566f93dc1f05df000f3ce8
dyda91 <edjamasalmeida@outlook.com>
2024-01-21 22:47:11 -0300

    fix: corrige importação das rotas

--- built/app.js
+++ built/app.js
@@ -8,8 +8,7 @@ var routes_1 = __importDefault(require("./routes"));
 var app = (0, express_1.default)();
 var PORT = 3000;
 app.use(express_1.default.json());
-// Configuração de outras middlewares, se necessário
-// Usando as rotas definidas no arquivo routes.ts
+
 app.use('/api', routes_1.default);
 app.listen(PORT, function () {
     console.log("Server is running on port ".concat(PORT));
