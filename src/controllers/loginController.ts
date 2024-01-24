88cc0e87f05486f532851f14bde9438552b423cc
RudsonSantana <rudsonsanttana@gmail.com>
2024-01-24 18:12:46 -0300

    refactor: resposta das verificações do login no controller

--- src/controllers/loginController.ts
+++ src/controllers/loginController.ts
@@ -1,12 +1,27 @@
 import { Request, Response, NextFunction } from 'express';
 import { loginCustomerService } from '../services/LoginService';
+import { customerRepository } from '../repositories/customerRepository';
+import { encrypt } from '../helpers/CryptHelper';
 
 class LoginController {
     async signIn(req: Request, res: Response, next: NextFunction) {
         try {
             const { email, password } = req.body;
+            const customer = await customerRepository.getCustomerByEmail(email);
+
+            if (!customer) {
+                res.status(400).json({ error: 'Email ou Senha inválidos' });
+                return next();
+            }
+
+            const passwordProvided = encrypt(password);
+            if (customer.password !== passwordProvided) {
+                res.status(400).json({ error: 'Email ou Senha inválidos' });
+                return next();
+            }
+
             const resposta = await loginCustomerService.signIn({ email, password });
-            res.send(resposta);
+            res.json(resposta);
             next();
         } catch (error) {
             console.error(error);
