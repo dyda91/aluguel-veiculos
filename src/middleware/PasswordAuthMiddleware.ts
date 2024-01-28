8a0b62e599f2963b0100824b99bafeaedc8dbb5d
RudsonSantana <rudsonsanttana@gmail.com>
2024-01-28 17:13:42 -0300

    feat: adicionando middlewares de validações de email, cpf, password, placa

---
+++ src/middleware/PasswordAuthMiddleware.ts
@@ -0,0 +1,28 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from 'jsonwebtoken';
+import { AppError } from "../errors/AppError";
+
+export const passwordAuthMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const authorizationHeader = req.header('Authorization');
+    
+    if (!authorizationHeader) {
+      throw new AppError('Token não informado');
+    }
+    
+    if (!authorizationHeader.toLocaleLowerCase().startsWith('bearer ')) {
+      throw new AppError('Token inválido');
+    }
+    
+    const accessToken = authorizationHeader.split(' ')[1];
+
+    const secret = process.env.JWT_SECRET_2!;
+    
+    jwt.verify(accessToken, secret);
+    next();
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Erro interno do servidor' });
+    next(error);
+  }
+}
