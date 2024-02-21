6102424e25efaf7d2726f9f5923543f1c37bd6a7
RudsonSantana <rudsonsanttana@gmail.com>
2024-02-21 14:13:59 -0300

    feat: adicionando migrations

---
+++ src/infra/db/sequelize/migrations/20240221142907-create-employeePositions.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('employeePositions', {
+      id: {
+        type: Sequelize.STRING(36),
+        primaryKey: true
+      },
+      name: {
+        type: Sequelize.STRING(16),
+        allowNull: false
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('employeePositions');
+  }
+};
