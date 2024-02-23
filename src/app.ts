e1e6341933b6a0a9b6cb96009acbd0b7e4eb2cb7
RudsonSantana <rudsonsanttana@gmail.com>
2024-02-23 13:39:08 -0300

    feat: adicionando o morgan, o helmet e o cors ao projeto

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,26 @@ dotenv.config();
 
 import express from 'express';
 import { routes } from './app/routes';
+import cors from 'cors';
+import morgan from 'morgan';
+import helmet from 'helmet';
 
 const port = process.env.PORT!;
 const app = express();
 
+app.use(cors({
+  origin: [
+    'http://127.0.0.1:5501',
+    'http://localhost:5501'
+  ]
+}));
+
+app.use(morgan('[:date[clf]] ":method :url" :status :res[content-length]'));
+
+app.use(helmet());
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 
