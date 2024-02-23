f5b950c686764975b06e3d978633bcd585dc7f1f
RudsonSantana <rudsonsanttana@gmail.com>
2024-02-23 13:33:09 -0300

    build: adicionando novas dependências

---
+++ src/infra/db/sequelize/migrations/20240221175844-create-employee.js
@@ -0,0 +1,51 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('employees', {
+      id: {
+        type: Sequelize.STRING(36),
+        primaryKey: true
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      cpf: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      phone: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      licenseCategory: {
+        type: Sequelize.STRING(36),
+        references: {
+          model: 'licenseCategories',
+          key: 'id'
+        },
+      },
+      position: {
+        type: Sequelize.STRING(36),
+        references: {
+          model: 'employeePositions',
+          key: 'id'
+        }
+      }
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('employees');
+  }
+};
