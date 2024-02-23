17e28fce1ca3e7e3e99fab3b7e16fe925493d67e
RudsonSantana <rudsonsanttana@gmail.com>
2024-02-23 14:21:09 -0300

    feat: formatos de respostas com handlebars e ejs para as rotas do Vehicle

--- src/app/routes/vehicleRoutes.ts
+++ src/app/routes/vehicleRoutes.ts
@@ -1,35 +1,41 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
 import { vehicleController } from "../controllers/VehicleController";
 import { authMiddleware } from "../middlewares/AuthMiddleware";
-import { existingVehicleMiddleware } from "../middlewares/ExistingVehicleMiddleware";
-import { vehicleCategoryMiddleware } from "../middlewares/VehicleCategoryMiddleware";
-import { validPlateFormatMiddleware } from "../middlewares/ValidPlateFormatMiddleware";
 import { authorizationByManagerMiddleware } from "../middlewares/AuthorizationByManagerMiddleware";
 import { authorizationByAttendantMiddleware } from "../middlewares/AuthorizationByAttendantMiddleware";
+import { validatePlateVehicleMiddleware } from "../middlewares/vehicle/ValidatePlateVehicleMiddleware";
+import { validPlateFormatMiddleware } from "../middlewares/vehicle/ValidatePlateFormatMiddleware";
+import { validateVehicleCategoryMiddleware } from "../middlewares/vehicle/ValidateVehicleCategoryMiddleware";
+import path from "path"
 
 const vehicleRoutes = Router();
 
+// Post
+vehicleRoutes.get('/vehicle', (req: Request, res: Response) => {
+    const caminho = path.resolve(__dirname, '..', 'views', 'vehicle.ejs');
+    res.render(caminho);
+});
+
+vehicleRoutes.post('/vehicles',
+    // authMiddleware,
+    // authorizationByManagerMiddleware.authorization,
+    validPlateFormatMiddleware.validate,
+    validatePlateVehicleMiddleware.validate,
+    validateVehicleCategoryMiddleware.validate,
+    vehicleController.create
+);
+
 // Get
-vehicleRoutes.get('/vehicles',
-    authMiddleware,
-    authorizationByAttendantMiddleware.authorization,
+vehicleRoutes.get('/vehicles/all',
+    // authMiddleware,
+    // authorizationByAttendantMiddleware.authorization,
     vehicleController.findAll
 );
 
 vehicleRoutes.get('/vehicles/:plate',
-    authMiddleware,
-    authorizationByAttendantMiddleware.authorization,
+    // authMiddleware,
+    // authorizationByAttendantMiddleware.authorization,
     vehicleController.findByPlate
-);
-
-// Post
-vehicleRoutes.post('/vehicles',
-    authMiddleware,
-    authorizationByManagerMiddleware.authorization,
-    existingVehicleMiddleware.check,
-    validPlateFormatMiddleware.validatePlate,
-    vehicleCategoryMiddleware.validateCategory,
-    vehicleController.create
 );
 
 export { vehicleRoutes };
