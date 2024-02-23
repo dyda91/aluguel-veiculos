674ea9d177fdc04859bce7b9dd38b941c42f3c52
RudsonSantana <rudsonsanttana@gmail.com>
2024-02-23 19:47:03 -0300

    feat: usando o ejs para as rotas de Login

--- src/app/routes/loginRoutes.ts
+++ src/app/routes/loginRoutes.ts
@@ -1,10 +1,23 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 import { loginController } from "../controllers/LoginController";
 import { customerLoginVerificationMiddleware } from '../middlewares/CustomerLoginVerificationMiddleware';
 import { employeeLoginVerificationMiddleware } from '../middlewares/EmployeeLoginVerificationMiddleware';
+import path from 'path';
 
 const loginRoutes = Router();
 
+//Get 
+loginRoutes.get('/login/customer', (req: Request, res: Response) => {
+    const caminho = path.resolve(__dirname, '..', 'views', 'loginCustomer.ejs');
+    res.render(caminho);
+});
+
+loginRoutes.get('/login/employee', (req: Request, res: Response) => {
+    const caminho = path.resolve(__dirname, '..', 'views', 'loginEmployee.ejs');
+    res.render(caminho);
+});
+
+//Post
 loginRoutes.post('/login/customer',
     customerLoginVerificationMiddleware.execute,
     loginController.signInCustomer
