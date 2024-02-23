cabfd492244ea9bd3860bd486ee8c539169919e2
RudsonSantana <rudsonsanttana@gmail.com>
2024-02-23 14:27:49 -0300

    refactor: middlewares de Customer

---
+++ src/app/middlewares/customer/ValidateCustomerCpfMiddleware.ts
@@ -0,0 +1,33 @@
+import { Request, Response, NextFunction } from 'express';
+import { customerRepository } from '../../../infra/db/sequelize/repositories/customerRepository';
+import bcrypt from 'bcrypt';
+
+class ValidateCustomerCpfMiddleware {
+    async validate(req: Request, res: Response, next: NextFunction) {
+        try {
+            const { cpf } = req.body;
+            const customer = await customerRepository.findByCpf(cpf);
+
+            if (customer) {
+                const compareCpf = await bcrypt.compare(cpf, customer.cpf);
+                if (compareCpf) {
+                    return res.status(400).json({ error: 'CPF já cadastrado' });
+                }
+            }
+
+            if (cpf.length !== 11) {
+                return res.status(400).json({ error: 'CPF deve ter 11 caracteres' });
+            }
+
+            next();
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Erro interno do servidor' });
+            next(error);
+        }
+    }
+}
+
+const validateCustomerCpfMiddleware = new ValidateCustomerCpfMiddleware();
+
+export { validateCustomerCpfMiddleware };
---
+++ src/app/middlewares/customer/ValidateCustomerEmailMiddleware.ts
@@ -0,0 +1,25 @@
+import { Request, Response, NextFunction } from 'express';
+import { customerRepository } from '../../../infra/db/sequelize/repositories/customerRepository';
+
+class ValidateCustomerEmailMiddleware {
+    async validate(req: Request, res: Response, next: NextFunction) {
+        try {
+            const { email } = req.body;
+            const customer = await customerRepository.findByEmail(email);
+
+            if (customer) {
+                return res.status(400).json({ error: 'Email já cadastrado' });
+            }
+
+            next();
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Erro interno do servidor' });
+            next(error);
+        }
+    }
+}
+
+const validateCustomerEmailMiddleware = new ValidateCustomerEmailMiddleware();
+
+export { validateCustomerEmailMiddleware }
