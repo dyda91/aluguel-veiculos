0359ee88a5dd301f8495c450fefa854834823d30
RudsonSantana <rudsonsanttana@gmail.com>
2024-02-24 13:33:34 -0300

    refactor: ajustando middlewares de Autenticação e validação de licença

--- src/app/middlewares/AuthMiddleware.ts
+++ src/app/middlewares/AuthMiddleware.ts
@@ -1,27 +1,37 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from 'jsonwebtoken';
 
-export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const authorizationHeader = req.header('Authorization');
-    
-    if (!authorizationHeader) {
-      return res.status(400).json({ error: 'Token não informado'});
-    }
-    
-    if (!authorizationHeader.toLocaleLowerCase().startsWith('bearer ')) {
-      return res.status(400).json({ error: 'Token não informado'});
-    }
-    
-    const accessToken = authorizationHeader.split(' ')[1];
+class AuthMiddleware {
+  async auth(req: Request, res: Response, next: NextFunction) {
+    try {
+      const authorizationHeader = req.header('Authorization');
 
-    const secret = process.env.JWT_SECRET!;
-    
-    jwt.verify(accessToken, secret);
-    next();
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Erro interno do servidor' });
-    next(error);
+      if (!authorizationHeader) {
+        return res.status(400).send({ error: 'Token Inválido' });
+      }
+
+      if (!authorizationHeader.toLocaleLowerCase().startsWith('bearer ')) {
+        return res.status(400).send({ error: 'Token Inválido' });
+      }
+
+      const accessToken = authorizationHeader.split(' ')[1];
+      const secret = process.env.JWT_SECRET!;
+
+      try {
+        jwt.verify(accessToken, secret);
+      } catch (error) {
+        return res.status(400).send({ error: 'Token Inválido' });
+      }
+
+      next();
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Erro interno do servidor' });
+      next(error);
+    }
   }
 }
+
+const authMiddleware = new AuthMiddleware();
+
+export { authMiddleware }
