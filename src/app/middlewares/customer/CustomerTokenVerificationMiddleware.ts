0f77940ef786e26d7450e604efce111109b7aa0c
RudsonSantana <rudsonsanttana@gmail.com>
2024-02-24 13:28:08 -0300

    refactor: ajustando middlewares de Customers

---
+++ src/app/middlewares/customer/CustomerTokenVerificationMiddleware.ts
@@ -0,0 +1,41 @@
+import { NextFunction, Request, Response } from "express";
+import { customerRepository } from "../../../infra/db/sequelize/repositories/customerRepository";
+import jwt from 'jsonwebtoken';
+import { log } from "handlebars";
+
+class CustomerTokenVerificationMiddleware {
+    async execute(req: Request, res: Response, next: NextFunction) {
+        try {
+            const authorizationHeader = req.header('Authorization');
+            const accessToken = authorizationHeader.split(' ')[1];
+            const secret = process.env.JWT_SECRET_2!;
+            const decodedToken: any = jwt.verify(accessToken, secret);
+
+            const id = decodedToken.id;
+            const email = decodedToken.email;
+            const cpf = decodedToken.cpf;
+
+            if (id && email && cpf) {
+                const customerId = await customerRepository.findById(id);
+                const customerEmail = await customerRepository.findByEmail(email);
+                const customer= await customerRepository.findByCpf(cpf)
+                const customerCPF = await cpf === await customer.cpf
+
+                if (!customerId || !customerEmail || !customerCPF) {
+                    return res.status(401).json({ error: 'Token inválido' });
+                } 
+
+                next();
+            }
+        }
+        catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Erro interno do servidor' });
+            next(error);
+        }
+    }
+}
+
+const customerTokenVerificationMiddleware = new CustomerTokenVerificationMiddleware();
+
+export { customerTokenVerificationMiddleware }
